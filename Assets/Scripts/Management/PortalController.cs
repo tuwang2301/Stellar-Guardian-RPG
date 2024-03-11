9a9955783fe2b5c9200bdd1961fde1d2321e0680
Dung <quangtu2301@gmail.com>
2024-03-11 15:19:34 +0700

    Fix portal bug

--- Assets/Scripts/Management/PortalController.cs
+++ Assets/Scripts/Management/PortalController.cs
@@ -1,22 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PortalController : Singleton<PortalController>
 {
-	protected override void Awake()
-	{
-		base.Awake();
-		this.gameObject.SetActive(false);
-	}
 
-	public void ShowPortal()
-	{
-		this.gameObject.SetActive(true);
-	}
+    [SerializeField] private string sceneToLoad;
+    [SerializeField] private string sceneTransitionName;
+
+    private float waitToLoadTime = 6f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        this.gameObject.SetActive(false);
+    }
+
+    public void ShowPortal()
+    {
+        this.gameObject.SetActive(true);
+    }
 
     public void HidePortal()
     {
         this.gameObject.SetActive(false);
     }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKey(KeyCode.F) && collision.gameObject.GetComponent<PlayerController>())
+        {
+            AudioManager.Instance.PLaySFX("scene");
+            SceneManagement.Instance.SetTransitionName(sceneTransitionName);
+            UIFade.Instance.FadeToBlack();
+            StartCoroutine(LoadSceneRoutine());
+        }
+    }
+
+    private IEnumerator LoadSceneRoutine()
+    {
+        while (waitToLoadTime >= 0)
+        {
+            waitToLoadTime -= Time.deltaTime;
+            yield return null;
+        }
+
+        if (sceneToLoad.Equals("Scene1-level2") || sceneToLoad.Equals("Scene1-level3"))
+        {
+            PlayerHealth.Instance.RESPAWN_SCENE = sceneToLoad;
+        }
+        PlayerPrefs.SetString("sceneToLoad", sceneToLoad);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
 }
